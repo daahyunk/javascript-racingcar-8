d12232e3e73c40b34d7402ffb1ee83201fdb7295
daahyunk <ekgusdl0408@naver.com>
2025-10-27 13:44:15 +0900

    chore: 불필요한 주석 제거

--- src/io/output.js
+++ src/io/output.js
@@ -5,7 +5,7 @@ function printRoundResult(cars) {
     const progress = '-'.repeat(position);
     MissionUtils.Console.print(`${name} : ${progress}`);
   });
-  MissionUtils.Console.print(''); // 라운드 간 줄바꿈
+  MissionUtils.Console.print('');
 }
 
 export default printRoundResult;
