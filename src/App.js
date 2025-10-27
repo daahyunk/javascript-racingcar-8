e71fd2e130a2f75018727f1dae13efa9de10be61
daahyunk <ekgusdl0408@naver.com>
2025-10-27 14:10:31 +0900

    feat: 최종 우승자 계산 및 출력 기능 통합

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import { readCarNames, readTryCount } from './io/input.js';
 import { validateCarNames, validateTryCount } from './utils/validator.js';
-import { initializeCars, playRound } from './core/racingGame.js';
-import printRoundResult from './io/output.js';
+import { initializeCars, playRound, getWinners } from './core/racingGame.js';
+import { printRoundResult, printWinners } from './io/output.js';
 
 class App {
   async run() {
@@ -14,13 +14,15 @@ class App {
       const tryCount = validateTryCount(tryCountInput);
 
       let cars = initializeCars(carNames);
-
       MissionUtils.Console.print('\n실행 결과');
 
       for (let i = 0; i < tryCount; i += 1) {
         cars = playRound(cars);
         printRoundResult(cars);
       }
+
+      const winners = getWinners(cars);
+      printWinners(winners);
     } catch (error) {
       MissionUtils.Console.print(error.message);
 
--- src/io/output.js
+++ src/io/output.js
@@ -1,6 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
-function printRoundResult(cars) {
+export function printRoundResult(cars) {
   cars.forEach(({ name, position }) => {
     const progress = '-'.repeat(position);
     MissionUtils.Console.print(`${name} : ${progress}`);
@@ -8,4 +8,7 @@ function printRoundResult(cars) {
   MissionUtils.Console.print('');
 }
 
-export default printRoundResult;
+export function printWinners(winners) {
+  const message = `최종 우승자 : ${winners.join(', ')}`;
+  MissionUtils.Console.print(message);
+}
